7cab46e955bbbcd9885b878bf302386649f5df9c
Karol <karol.zerna@gmail.com>
2021-03-17 02:02:12 -0400

    travel_path refactored

--- mario_world.py
+++ mario_world.py
@@ -3,9 +3,10 @@ import queue
 #import Queue as queue
 
 class World:
-    def __init__(self, map, settings):
+    def __init__(self, map, settings, agent):
         self.map = map
         self.settings = settings
+        self.agent = agent
         (self.rows, self.columns) = self.map.shape
 
 
@@ -20,7 +21,10 @@ class World:
             if self.map[x][y] == self.settings.PIPE:
                 return path
         
-            for x2, y2 in ((x+1,y), (x-1,y), (x,y+1), (x,y-1)):
+            actions = [self.settings.UP, self.settings.DOWN, self.settings.LEFT, self.settings.RIGHT]
+            successors = self.agent.transition_function(x, y, actions)
+
+            for x2,y2 in successors:
                 if 0 <= x2 < self.rows and 0 <= y2 < self.columns and self.map[x2][y2] != self.settings.WALL and (x2, y2) not in seen:
                     queue.append(path + [(x2, y2)])
                     seen.add((x2, y2))
