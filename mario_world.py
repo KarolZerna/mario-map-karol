f4a0416ff9205b4566cf33098a1483e0fd233a43
Karol <karol.zerna@gmail.com>
2021-03-17 01:09:23 -0400

    travel path modified

--- mario_world.py
+++ mario_world.py
@@ -1,43 +1,26 @@
+import collections
 import queue
 #import Queue as queue
 
 class World:
-    def __init__(self, map, settings, agent):
+    def __init__(self, map, settings):
         self.map = map
         self.settings = settings
-        self.agent = agent
         (self.rows, self.columns) = self.map.shape
 
-    def discard_successors(self, successors):
-        filtered_successors = []
-        for successor in successors:
-            i = successor[0]
-            j = successor[1]
-            if 0 <= i < self.rows and  0 <= j < self.columns and self.map[i][j] == self.settings.VISITED:
-                filtered_successors.append(successor)
-        return filtered_successors
 
-    def travel_path_bfs(self, i, j):
-        open = queue.SimpleQueue()
-        close = []
-        open.put([i, j])
-        while open.qsize() != 0:
-            state = open.get()
-            # mark it as visited
-            self.map[state[0]][state[1]] = self.settings.ROAD
-            print(self.map, "\n")
-            actions = [self.settings.UP, self.settings.DOWN, self.settings.LEFT, self.settings.RIGHT]
-            successors = self.agent.transition_function(state, actions)
-            successors = self.discard_successors(successors)
-            close.append(state)
-            for successor in successors:
-                open.put(successor)
+    def travel_path_bfs(self, start):
+        queue = collections.deque([[start]]) 
+        seen = set([start])
 
-    def number_of_box(self):
-        boxes = 0
-        for i in range(self.rows):
-            for j in range(self.columns):
-                if self.map[i][j] == self.settings.MARIO:
-                    self.travel_path_bfs(i, j)
-                    boxes += 1
-        return boxes
+        while queue:
+            path = queue.popleft()
+            x, y = path[-1]
+
+            if self.map[x][y] == self.settings.PIPE:
+                return path
+        
+            for x2, y2 in ((x+1,y), (x-1,y), (x,y+1), (x,y-1)):
+                if 0 <= x2 < self.rows and 0 <= y2 < self.columns and self.map[x2][y2] != self.settings.WALL and (x2, y2) not in seen:
+                    queue.append(path + [(x2, y2)])
+                    seen.add((x2, y2))
