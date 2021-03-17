efbb8a632d9ddfe70c192e563e59c09976198a34
Karol <karol.zerna@gmail.com>
2021-03-17 00:36:33 -0400

    settings

--- settings.py
+++ settings.py
@@ -3,8 +3,5 @@ class Settings:
     DOWN = 'DOWN'
     LEFT = 'LEFT'
     RIGHT = 'RIGHT'
-    MARIO = 1 
-    PIPE = 2 #tubería
-    WALL = 3 #muro
-    VISITED = 0 #camino
-    ROAD = 8
+    WALL = 1 
+    PIPE = 2
